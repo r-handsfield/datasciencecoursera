79d031c40d3df542cf867a45b7b896cbbba79eef
r-handsfield <r.handsfield@gmail.com>
2015-11-11 01:20:06 -0500

    Updates Quizzes/server.r

--- 9-developingDataProducts/Quizzes/server.r
+++ 9-developingDataProducts/Quizzes/server.r
@@ -4,7 +4,7 @@ data(galton)
 shinyServer(
 	function(input, output) {
 		output$newHist <- renderPlot({
-			hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
+			hist(galton$child, xlab='child height', col='lightblue, main='Histogram')
 			mu <- input$mu
 			lines(c(mu, mu), c(0, 200),col="red",lwd=5)
 			mse <- mean((galton$child - mu)^2)
