c42c517451a890dea99b5c6aa99da8788dc8ecf8
r-handsfield <r.handsfield@gmail.com>
2015-11-19 19:23:59 -0500

    completes lissajous project for c9w4p1

--- 9-developingDataProducts/courseProject/server.r
+++ 9-developingDataProducts/courseProject/server.r
@@ -1,5 +1,6 @@
 # server.r
 # calculates a lissajous curve
+######################################################
 
 library(shiny)
 
@@ -7,61 +8,27 @@ t <- seq(0, 2*pi, .01)
 
 shinyServer(
 	function(input, output) {
-# 		
-# 		reactive({
-# 			output$a <- input$slider1
-# 		})
-# 		
-# 		reactive({
-# 			output$b <- input$slider2
-# 		})		
 		
+		# use value via A(), B(), etc.
+		A <- reactive({ input$slider1 })
 		
-		output$text1 <- renderText({paste("You have selected this")})
-		output$text2 <- renderText({paste(input$slider1, '- ', input$slider2)})
+		B <- reactive({ input$slider2 })		
 		
+		# show slider values
+		output$text1 <- renderText({paste0("You have selected A=", A(), " and B=", B() )})
+		
+		# plot the lissajous curve
 		output$plot <- renderPlot({
 			#lissajous stuff goes herex
-			plot(x=sin(t*input$slider1), y=sin(t*input$slider2), xlab='X', col='red', type='l', main='Lissajous')
+			plot(
+				x=sin(t*A()), 
+				y=sin(t*B()), 
+				xlab=paste('sin(', A(), ' * t )'),
+				ylab=paste('sin(', B(), ' * t )'),
+				col='red', type='l', lwd=3, 
+				main='Lissajous Curve  (Y vs X)'
+			)
 		})
 
-		output$blank <- renderPrint({c("Lorem ipso ballsac", a, input$slider1, input$slider2)})
-
-# 		output$table <- renderTable({(1:6)})
 	}
 )
-
-
-
-# shinyServer(function(input, output) {
-# 	
-# 	data <- reactive({
-# 		dist <- switch(input$dist,
-# 			       norm = rnorm,
-# 			       unif = runif,
-# 			       lnorm = rlnorm,
-# 			       exp = rexp,
-# 			       rnorm)
-# 		
-# 		dist(input$n)
-# 	})
-# 	
-# 	output$plot <- renderPlot({
-# 		dist <- input$dist
-# 		n <- input$n
-# 		
-# 		hist(data(), 
-# 		     main=paste('r', dist, '(', n, ')', sep=''))
-# 	})
-# 	
-# 	# Generate a summary of the data
-# 	output$summary <- renderPrint({
-# 		summary(data())
-# 	})
-# 	
-# 	# Generate an HTML table view of the data
-# 	output$table <- renderTable({
-# 		data.frame(x=data())
-# 	})
-# 	
-# })
