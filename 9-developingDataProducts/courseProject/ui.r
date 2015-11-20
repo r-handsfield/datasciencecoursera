b7e50ed8ee196a31ad83612b3509ee607b820ed8
r-handsfield <r.handsfield@gmail.com>
2015-11-19 19:56:07 -0500

    renames ui.R to ui.r

---
+++ 9-developingDataProducts/courseProject/ui.r
@@ -0,0 +1,46 @@
+# ui.r
+# displays a lissajous curve
+######################################################
+
+
+library(shiny);
+
+shinyUI(fluidPage(
+	titlePanel(
+		h1("Lissajous Curves")	   
+   	),
+
+	# Left Sidebar with controls
+	sidebarLayout(
+		sidebarPanel(
+		
+			HTML("<h4><font color='red'>Use the sliders to choose values for A and B to plot a Lissajous curve.</font></h4>"),
+			
+			sliderInput(inputId="slider1", label="A", min=0, max=12, value=3, step=1),	
+			
+			sliderInput(inputId="slider2", label="B", min=0, max=12, value=6, step=1),
+			
+			br(),
+			br(),
+			
+			p("A and B represent the frequencies of two oscillators."),
+			p("To form closed curves, try values like {1,2}, {2,3}, and {4,5}."),
+			p("Form open curves with values like {1,3}, {2,6}, and {3,7}."),
+			p("Closed Lissajous curves describe the interference between notes in a musical scale.")
+# 			
+# 			actionButton("action", label = "Action")
+		),
+	
+		# Main Panel with Display Tabs (still within `sidebarLayout()` )
+		mainPanel(
+			HTML("<h5>A <a href=https://en.wikipedia.org/wiki/Lissajous_curve>Lissajous curve</a> describes the relationship between two oscillators vibrating simultaneously. Equations for a Lissajous curve can take the simplified form:</h5>"),
+			
+			HTML("<pre><h5>X = sin(A*t)          Y = sin(B*t)</h5></pre>"),
+			
+			br(),
+			
+			textOutput("text1"),
+		    plotOutput("plot")
+		) 
+	)
+));
