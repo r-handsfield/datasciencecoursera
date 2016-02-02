fc0ded0fc94d1c8b61793c8522d8266c44e17d33
r-handsfield <r.handsfield@gmail.com>
2016-02-02 13:19:56 -0500

    Completes c6w4l1

---
+++ 6-statisticalInference/Lectures/week4/sources/import_stylesheet.R
@@ -0,0 +1,9 @@
+# script to import custom css into Rmd Doc
+options(rstudio.markdownToHTML = 
+		function(inputFile, outputFile) {      
+			require(markdown)
+			markdownToHTML(inputFile, outputFile, stylesheet='custom.css')   
+		}
+)
+
+print("It did")
